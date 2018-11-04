43cb4bcdf47e3656c282f4afad67fa52a1081765
Deonte Williams <icode4lyfe@gmail.com>
2018-11-04 04:39:48 -0500

    scrolls to bottom if user is close to bottom

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,5 +1,19 @@
 let socket = io();
 
+function scrollToBottom(params) {
+   let messages = $("#messages");
+   let newMessage = messages.children("li:last-child");
+   let clientHeight = messages.prop("clientHeight");
+   let scrollTop = messages.prop("scrollTop");
+   let scrollHeight = messages.prop("scrollHeight");
+   let newMessageHeight = newMessage.innerHeight();
+   let lastMessageHeight = newMessage.prev().innerHeight();
+
+   if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+      messages.scrollTop(scrollHeight);
+   }
+}
+
 socket.on("connect", function() {
    console.log("Connected to server.");
 
@@ -15,6 +29,7 @@ socket.on("newMessage", function(message) {
    });
 
    $("#messages").append(html);
+   scrollToBottom();
 });
 
 socket.on("disconnect", function() {
@@ -31,6 +46,7 @@ socket.on("newLocationMessage", function(message) {
    });
 
    $("#messages").append(html);
+   scrollToBottom();
 });
 
 $("#message-form").on("submit", function(e) {
